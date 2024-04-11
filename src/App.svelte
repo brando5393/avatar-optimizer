c8c743b999d73fd940946f85d8eed1abebebad65
Brandon Williams <brando5393@gmail.com>
2024-04-10 21:33:46 -0500

    some custom boilerplate in place

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,30 @@
-<h1 class="text-2xl font-bold">Hello World!</h1>
+<header>
+  <!-- Create section with an id of appHeader along with an h1 for the app name and an h2 for the tagline styled with tailwind css classes use placeholder text -->
+  <section id="appHeader">
+    <h1 class="text-4xl font-bold text-center">SvelteKit</h1>
+    <h2 class="text-2xl text-center">The next big thing in web development</h2>
+  </section>
+</header>
+<main>
+  <!--Create a section called appCContent-->
+  <section id="appContent">
+    <div class="container"></div>
+  </section>
+</main>
+<footer>
+  <!-- Use the grid system to make divs for a 3 column footer design -->
+  <div class="grid grid-cols-3">
+    <div>
+      <h3>Column 1</h3>
+      <p>Some text</p>
+    </div>
+    <div>
+      <h3>Column 2</h3>
+      <p>Some text</p>
+    </div>
+    <div>
+      <h3>Column 3</h3>
+      <p>Some text</p>
+    </div>
+  </div>
+</footer>
