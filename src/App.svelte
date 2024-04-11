322781420eac4cdb141a816100942f6a36b0f90a
Brandon Williams <brando5393@gmail.com>
2024-04-10 21:43:04 -0500

    A few more changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <header>
   <!-- Create section with an id of appHeader along with an h1 for the app name and an h2 for the tagline styled with tailwind css classes use placeholder text -->
-  <section id="appHeader">
+  <section id="appHeader" class="jumbotron">
     <h1 class="text-4xl font-bold text-center">SvelteKit</h1>
     <h2 class="text-2xl text-center">The next big thing in web development</h2>
   </section>
@@ -8,7 +8,10 @@
 <main>
   <!--Create a section called appCContent-->
   <section id="appContent">
-    <div class="container"></div>
+    <div class="container">
+      Eum est dicta odit voluptates quia. Dolor eos debitis recusandae facere.
+      Animi aut minima est labore nemo unde. Aut quis sit.
+    </div>
   </section>
 </main>
 <footer>
